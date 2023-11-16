8d98a0a0c41c74659f6eb0529211dc7bb071c24a
Anju-Kurian-08 <150518861+Anju-Kurian-08@users.noreply.github.com>
2023-11-16 07:27:07 +0530

    helloworld

---
+++ sampleproject2/src/main/java/samplepackage2/Hello_World_Print.java
@@ -0,0 +1,10 @@
+package samplepackage2;
+
+public class Hello_World_Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("this is my sample maven project cloned to github");
+	}
+
+}
